cb43a3d671284fb27f81c7a96d874892d1de8044
Branon Eusebio <50970682+BranonConor@users.noreply.github.com>
2019-07-12 16:38:47 -0700

    JS Code Update
    - Added final functionalities

--- ToDo List Project/assets/js/todoList.js
+++ ToDo List Project/assets/js/todoList.js
@@ -1,11 +1,11 @@
 
 //Check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete a Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	//target the whole li, not just the span
 	$(this).parent().fadeOut(300, function() {
 		//remove the whole thing
@@ -15,3 +15,17 @@ $("span").click(function(event) {
 	event.stopPropagation();
 });
 
+//
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text']").fadeToggle(500);
+});
